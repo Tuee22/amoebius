92b6282b51db41330355b2305df9a08fc005c187
Matt Nowak <17482065+Tuee22@users.noreply.github.com>
2024-09-18 15:38:40 -0400

    changes

--- minikube/main.tf
+++ minikube/main.tf
@@ -1,44 +1,29 @@
-terraform {
-  required_providers {
-    minikube = {
-      source  = "scott-the-programmer/minikube"
-      version = "0.4.0"
-    }
-  }
-}
+# Root module configuration
 
-provider "minikube" {
-  kubernetes_version = "v1.31.0"
-}
+provider "local" {}
 
-# Main Minikube cluster resource
-resource "minikube_cluster" "terraform-cluster" {
-  driver       = "docker"
+module "minikube_cluster" {
+  source = "./modules/minikube"  # Path to your generic module
+
+  # Pass the required inputs
   cluster_name = var.cluster_name
-  addons       = ["default-storageclass", "storage-provisioner"]
+  local_folder = var.local_folder
+  mount_folder = var.mount_folder
 }
 
-# This resource starts Minikube (idempotent)
-resource "null_resource" "start_minikube" {
-  depends_on = [minikube_cluster.terraform-cluster]
-
-  provisioner "local-exec" {
-    command = <<-EOF
-      minikube -p ${var.cluster_name} stop
-      minikube -p ${var.cluster_name} start
-      nohup minikube mount -p ${var.cluster_name} ${var.local_folder}:${var.mount_folder} > minikube-mount.log 2>&1 &
-    EOF
-  }
-  
-  // trigger so we always restart and mount minikube on terraform apply
-  triggers = {
-    start_minikube = "${timestamp()}"
-  }
-}
-
+# Use the Kubernetes provider from the Minikube cluster setup
 provider "kubernetes" {
-  host = minikube_cluster.terraform-cluster.host
-  client_certificate     = minikube_cluster.terraform-cluster.client_certificate
-  client_key             = minikube_cluster.terraform-cluster.client_key
-  cluster_ca_certificate = minikube_cluster.terraform-cluster.cluster_ca_certificate
+  host = module.minikube_cluster.host
+  client_certificate     = module.minikube_cluster.client_certificate
+  client_key             = module.minikube_cluster.client_key
+  cluster_ca_certificate = module.minikube_cluster.cluster_ca_certificate
 }
+
+# Example Kubernetes resource using the Kubernetes provider configured by the module
+resource "kubernetes_namespace" "example" {
+  metadata {
+    name = "example-namespace"
+  }
+}
+
+# Additional Kubernetes resources could go here...
---
+++ terraform-modules/minikube/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    minikube = {
+      source  = "scott-the-programmer/minikube"
+      version = "0.4.0"
+    }
+  }
+  kubernetes = {
+    source  = "hashicorp/kubernetes"
+  }
+}
+
+provider "minikube" {
+  kubernetes_version = "v1.31.0"
+}
+
+# Main Minikube cluster resource
+resource "minikube_cluster" "terraform-cluster" {
+  driver       = "docker"
+  cluster_name = var.cluster_name
+  addons       = ["default-storageclass", "storage-provisioner"]
+}
+
+# This resource starts Minikube (idempotent after restart)
+# NB: the stop/start is to account for 
+resource "null_resource" "start_minikube" {
+  depends_on = [minikube_cluster.terraform-cluster]
+
+  provisioner "local-exec" {
+    command = <<-EOF
+      minikube -p ${var.cluster_name} stop
+      minikube -p ${var.cluster_name} start
+      nohup minikube mount -p ${var.cluster_name} ${var.local_folder}:${var.mount_folder} > minikube-mount.log 2>&1 &
+    EOF
+  }
+  
+  // trigger so we always restart and mount minikube on terraform apply
+  triggers = {
+    start_minikube = "${timestamp()}"
+  }
+}
+
+provider "kubernetes" {
+  host = minikube_cluster.terraform-cluster.host
+  client_certificate     = minikube_cluster.terraform-cluster.client_certificate
+  client_key             = minikube_cluster.terraform-cluster.client_key
+  cluster_ca_certificate = minikube_cluster.terraform-cluster.cluster_ca_certificate
+}
