636ffb02fc8409cddfc3e6495a861d8edbfb4f28
Matt Nowak <17482065+Tuee22@users.noreply.github.com>
2025-02-21 12:07:32 -0500

    changed default azure zone

--- python/amoebius/models/providers/deploy/azure.py
+++ python/amoebius/models/providers/deploy/azure.py
@@ -9,19 +9,22 @@ from amoebius.models.cluster_deploy import ClusterDeploy, InstanceGroup
 class AzureClusterDeploy(ClusterDeploy):
     def __init__(
         self,
-        region: str = "westus2",
+        region: str = "eastus2",
         vpc_cidr: str = "10.0.0.0/16",
         availability_zones: List[str] = ["1", "2", "3"],
         instance_type_map: Dict[str, str] = {
-            "arm_small": "Standard_D2ps_v5",
-            "arm_medium": "Standard_D4ps_v5",
-            "arm_large": "Standard_D8ps_v5",
-            "x86_small": "Standard_D2s_v5",
-            "x86_medium": "Standard_D4s_v5",
-            "x86_large": "Standard_D8s_v5",
-            "nvidia_small": "Standard_NC4as_T4_v3",
-            "nvidia_medium": "Standard_NC6s_v3",
-            "nvidia_large": "Standard_ND96amsr_A100_v4",
+            # ARM-based
+            "arm_small": "Standard_D2ps_v5",  # 2 vCPUs, 8 GiB
+            "arm_medium": "Standard_D4ps_v5",  # 4 vCPUs, 16 GiB
+            "arm_large": "Standard_D8ps_v5",  # 8 vCPUs, 32 GiB
+            # x86-based
+            "x86_small": "Standard_D2s_v5",  # 2 vCPUs, 8 GiB
+            "x86_medium": "Standard_D4s_v5",  # 4 vCPUs, 16 GiB
+            "x86_large": "Standard_D8s_v5",  # 8 vCPUs, 32 GiB
+            # NVIDIA GPU
+            "nvidia_small": "Standard_NC4as_T4_v3",  # 4 vCPUs, T4 GPU
+            "nvidia_medium": "Standard_NC6s_v3",  # 6 vCPUs, V100 GPU
+            "nvidia_large": "Standard_NC24s_v3",  # 24 vCPUs, V100 GPU
         },
         arm_default_image: str = "Canonical:0001-com-ubuntu-server-jammy:22_04-lts-arm64:latest",
         x86_default_image: str = "Canonical:0001-com-ubuntu-server-jammy:22_04-lts:latest",
@@ -30,7 +33,7 @@ class AzureClusterDeploy(ClusterDeploy):
         vault_role_name: str = "amoebius-admin-role",
         no_verify_ssl: bool = True,
     ):
-        # If an InstanceGroup has image=None, fill with either ARM or x86 default:
+        # Fill in default images if none specified
         for ig in instance_groups:
             if ig.image is None:
                 if ig.category.startswith("arm_"):
